f012364f416ed7b41d3ad9764f32b3b8ed47e479
Edward Rodriguez <rodriguez.avery@gmail.com>
2023-06-02 00:31:38 -0500

    Complete problems 1-4

--- final_practice.rb
+++ final_practice.rb
@@ -6,30 +6,76 @@
 
 # 1: Write a method named greeting that returns a string with a general greeting. 
 
-# What is the return value of your method?
-# How many arguments did you pass your method?
+def greeting
+    return "Hello neighbor"
+end
 
+puts greeting
+
+
+# What is the return value of your method?
+# The return value of my method is Hello neighbor
+
+# How many arguments did you pass your method?
+# 0
 
 # 2: Write a method named custom_greeting that returns a greeting WITH a specific name.
 
+def custom_greeting(name)
+    return "Hey #{name} How are you?"
+end
+
+puts custom_greeting("Nate")
+puts custom_greeting("Mark")
+
 # What is the return value of your method?
+# The return value of my method is "Hey #{name} How are you?"
+
 # How many arguments did you pass your method?
+# I passed 2 argumetns through my method
+
 # What data type was your argument(s)?
+# My arguments were strings
 
 
 # 3: Write a method named greet_person that takes in 3 strings, a first, middle, and last name, and returns a sentence with the full name.
 
+def greet_person (first, middle, last)
+    return "Hey #{first} #{middle} #{last}. Whats up?"
+end
+
+puts greet_person("Edward", "A", "Rodriguez")
+
+
 # What is the return value of your method?
+# The return value on my method was "Hey Edward A Rodriguez. Whats up?"
+
 # How many arguments did you pass your method?
+# I passed 3 arguements through my method
+
 # What data type was your argument(s)?
+# My arguements were Strings.
 
 
 # 4: Write a method named square that takes in one integer, and returns the square of that integer.
 # Bonus: Print a sentence that interpolates the return value of your square method.
 
+def square (number)
+    return number * number 
+end
+
+puts square(12)
+puts square(7)
+
+
 # What is the return value of your method?
+# The return value of my method was 144, and 49.
+
 # How many arguments did you pass your method?
+# I passed 2 different arguments through my method
+
 # What data type was your argument(s)?
+# My arguments were integers
 
 
 # 5: Write a method named check_stock that satisfies the following interaction pattern:
