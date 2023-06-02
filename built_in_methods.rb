b0270931fa382753150ad2b82c4b0e0d1de0f00a
Edward Rodriguez <rodriguez.avery@gmail.com>
2023-06-01 23:39:25 -0500

    Complete all 4 sections in the methods file

--- built_in_methods.rb
+++ built_in_methods.rb
@@ -48,6 +48,22 @@
 first_name = "Jeff"
 puts first_name.start_with?("J")
 
+# The length method is called on the city_state variable, which stores the string object "Little Rock, AR"
+# The length method returns the value of the number of characters in the string object.
+# In this example, the return value is 15 because "Little Rock, AR" has 15 characters.
+# The puts command prints the return value of nil to the console. 
+city_state = "Little Rock, AR"
+puts city_state.length
+
+# The empty? method is called on the ive_got_nothing varibale, which stores the string object ""
+# The empty? method returns true if variable.length == 0
+# In this example, the return value is true because "" has no characters.
+# The puts command prints the return value of the empty? method (true) to the console.
+# It also prints nil to the console
+
+ive_got_nothing = ""
+puts ive_got_nothing.empty?
+
 
 
 # SECTION 3: Calling methods on variables assigned to integers.
@@ -56,6 +72,18 @@ puts first_name.start_with?("J")
 # https://ruby-doc.org/core-3.1.0/Integer.html
 # Include comments above each method call explaining the impact and return value of that method.
 
+# The even? method is called on the age variable which stores the integer 30. The even? method returns true if the variable is equal to an even number.
+# In this example the return value of even? is true because age = 30.
+# The puts command prints the return value nil to the console.
+age = 30
+puts age.even?
+
+# The odd? method is called on the height variable which stores the integer 72. The odd? method returns true if the variable is equal to an odd number.
+# In this example the return value of odd? is false because height = 72.
+# The puts command prints the return value nil to the console.
+height = 72
+puts height.odd?
+
 
 
 # SECTION 4: Calling methods on variables assigned to arrays.
@@ -63,3 +91,13 @@ puts first_name.start_with?("J")
 # Call a different built-in Ruby method on each of your variables. 
 # https://ruby-doc.org/core-3.1.0/Array.html
 # Include comments above each method call explaining the impact and return value of that method.
+
+#  The count method is called on the friends array which stores 3 strings. The count methond counts the number of strings in the array and returns that value, in this example it returns 3.
+# The puts command also prints the return value nil to the console
+friends = ["Mark", "Nate", "Bobby"]
+puts friends.count
+
+# The shift method is called on the enemies array which stores 3 strings. The shift method removes the first element of the array and returns its value. In this example it removes Nate from the array and returns Nate to the console.
+# The puts command also prints the return value bil to the console.
+enemies = ["Nate", "Graham", "The number 6"]
+puts enemies.shift
